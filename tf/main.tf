d9042f0ce6f5c2bb42930888947ddf04b69034f2
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-09 13:12:19 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -298,7 +298,6 @@ sudo dnf update -y
 # GitLab 패키지 리포지토리 추가 (Community Edition)
 curl "https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh" | sudo bash
 
-
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
 HOSTNAME=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/public-hostname)
 echo ${HOSTNAME}
