1e2a4b759a217a154f5142078f9f4f8c3a1de555
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-09 13:17:39 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -299,9 +299,7 @@ sudo dnf update -y
 curl "https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh" | sudo bash
 
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-HOSTNAME=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/public-hostname)
-
-export EXTERNAL_URL="${HOSTNAME}"
+export EXTERNAL_URL="$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/public-hostname)"
 dnf install -y gitlab-ce
 _DATA
 
