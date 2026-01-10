af2dfef11d9459ce1cabad2edf1cc601a29615de
gnosia93 <45726390+gnosia93@users.noreply.github.com>
2026-01-10 22:42:28 +0900

    Update main.tf

--- tf/main.tf
+++ tf/main.tf
@@ -222,6 +222,7 @@ sleep 5
 sed -i 's/127.0.0.1:8080/0.0.0.0:9090/g; s/auth: password/auth: none/g' /home/ec2-user/.config/code-server/config.yaml
 EC2_USER_SCRIPT
 
+echo 'export PS1="$(uname -m) \$ "' >> /home/ec2-user/.bashrc
 sudo systemctl restart code-server@ec2-user
 _DATA
 
@@ -255,6 +256,7 @@ sleep 5
 sed -i 's/127.0.0.1:8080/0.0.0.0:9090/g; s/auth: password/auth: none/g' /home/ec2-user/.config/code-server/config.yaml
 EC2_USER_SCRIPT
 
+echo 'export PS1="$(uname -m) \$ "' >> /home/ec2-user/.bashrc
 sudo systemctl restart code-server@ec2-user
 _DATA
 
